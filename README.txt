09ab85cfeb9f04aa429718578d9b9a3dbdc60ddf
chaochen <chao.chen@eficode.com>
2012-04-12 15:14:42 +0800

    basic installation and configure UI feature implementation

---
+++ README.txt
@@ -0,0 +1,10 @@
+Installation and usage:
+
+installation
+1.Manage Jenkins->Manage Plugins->Advanced->Choose File(Upload Plugin)->Upload
+the plugin file path: Deveoplugin/target/Deveoplugin.hpi
+
+usage
+2.Click into a project -> Configure ->Click the "Build" configuration item->Report to Deveo
+
+you can specify the apiurl and company bot and if report the status in processing. 
---
+++ src/main/java/deveo/jenkins/plugin/DeveoPlugin.java
@@ -0,0 +1,116 @@
+package deveo.jenkins.plugin;
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.util.FormValidation;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.AbstractProject;
+import hudson.tasks.Builder;
+import hudson.tasks.Recorder;
+import hudson.tasks.BuildStepDescriptor;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class DeveoPlugin extends Builder {
+
+    private final String apiurl;
+    private final String bot;
+    private final String access;
+
+    private boolean continuous_reports;
+    private final String script = null;
+
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public DeveoPlugin(String access, String apiurl, String bot) {
+        this.access = access;
+        this.apiurl = apiurl;
+        this.bot = bot;
+    }
+
+    //We'll use this from the <tt>config.jelly</tt>.
+    public String getAccess() {
+        return this.access;
+    }
+    public String getApiUrl() {
+        return this.apiurl;
+    }
+    public String getBot() {
+        return this.bot;
+    }
+    public boolean getContinuous(){
+        return this.continuous_reports;
+    }
+    
+    
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+            
+        return true;
+    }
+    
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+    
+
+
+    @Extension 
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        private boolean useFrench;
+        private boolean continuousReport;
+        
+        public FormValidation doCheckAccess(@QueryParameter String value)
+                throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set a access");
+            return FormValidation.ok();
+        }
+        
+        public FormValidation doCheckBot(@QueryParameter String value)
+        throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set a bot");
+            return FormValidation.ok();
+        }
+        
+        public FormValidation doCheckApiurl(@QueryParameter String value)
+        throws IOException, ServletException {
+            if (value.length() == 0)
+                return FormValidation.error("Please set a access");
+//            if (value.length() < 4)
+//                return FormValidation.warning("Isn't the name too short?");
+            return FormValidation.ok();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            continuousReport = formData.getBoolean("continuousReport");
+            save();
+            return super.configure(req,formData);
+        }
+
+        public boolean getContinuous() {
+            return continuousReport;
+        }
+        
+        
+        
+        
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+        
+        public String getDisplayName() {
+            return "Report to Deveo";
+        }
+    }
+}
+
